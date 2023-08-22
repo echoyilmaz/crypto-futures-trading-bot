003ebfd6e4ee52fb53f6f0f7c9ade09879159df5
Shisho <shishohf@shisho.net>
2023-08-22 02:50:45 +0000

    Added API functions for OI and L/S ratios

--- utils/api.py
+++ utils/api.py
@@ -11,10 +11,14 @@ async def fetch_pairs(session):
             pairs = []
             for contract in data['data']['contracts']:
                 pair = str(contract['symbol'])
-                if 'USDT' in pair and contract['maxLongLeverage'] >= 5 and contract['maxShortLeverage'] >= 5:
+                if 'USDT' in pair and contract['maxLongLeverage'] >= 20 and contract['maxShortLeverage'] >= 20:
                     pair_entry = {'bingx': pair, 'binance': None, 'depth': None}
                     pair = ''.join(filter(lambda x: x not in '-', str(pair)))
                     pair_entry.update({'binance': pair})
+                    # symbol_list = ["BTCUSDT"]
+                    # if pair_entry['binance'] not in symbol_list:
+                    #     continue
+                    print(pair_entry['binance'])
                     async with session.get("https://fapi.binance.com/fapi/v1/exchangeInfo") as depth_request:
                         if depth_request.status == 200:
                             depth_data = await depth_request.json()
@@ -54,6 +58,65 @@ async def fetch_candles(session, pair, interval, long_tf):
                     prices.rename(columns={0: 'open_time', 1: 'open', 2: 'high', 3: 'low', 4: 'close', 5: 'volume', 6: 'close_time'}, inplace=True)
 
                     return prices
+                
+        elif request.status == 429:
+            print("API limit broken")
+            await asyncio.sleep(60)
+        elif request.status == 419:
+            print("IP banned from API endpoint")
+        else:
+            print(f"Some other response code for {pair['binance']}?")
+
+async def fetch_open_interest(session, pair, interval, long_tf):
+    params = {"symbol": pair['binance'], "period": interval, "limit": long_tf}
+    
+    async with session.get("https://fapi.binance.com/futures/data/openInterestHist", params=params, timeout=5) as request:
+        if request.status == 200:
+            data = await request.json(content_type=None)
+            
+            open_interest_data = [
+                {
+                    'symbol': entry['symbol'],
+                    'sumOpenInterest': float(entry['sumOpenInterest']),
+                    'sumOpenInterestValue': float(entry['sumOpenInterestValue']),
+                    'timestamp': pd.Timestamp(entry['timestamp'], unit='ms')
+                }
+                for entry in data
+            ]
+            
+            if open_interest_data:
+                open_interest = pd.DataFrame(open_interest_data)
+                return open_interest
+                
+        elif request.status == 429:
+            print("API limit broken")
+            await asyncio.sleep(60)
+        elif request.status == 419:
+            print("IP banned from API endpoint")
+        else:
+            print(f"Some other response code for {pair['binance']}?")
+
+async def fetch_long_short_ratio(session, pair, interval, long_tf):
+    params = {"symbol": pair['binance'], "period": interval, "limit": long_tf}
+    
+    async with session.get("https://fapi.binance.com/futures/data/globalLongShortAccountRatio", params=params, timeout=5) as request:
+        if request.status == 200:
+            data = await request.json(content_type=None)
+            
+            long_short_data = [
+                {
+                    'symbol': entry['symbol'],
+                    'longShortRatio': float(entry['longShortRatio']),
+                    'longAccount': float(entry['longAccount']),
+                    'shortAccount': float(entry['shortAccount']),
+                    'timestamp': pd.Timestamp(entry['timestamp'], unit='ms')
+                }
+                for entry in data
+            ]
+            
+            if long_short_data:
+                long_short_ratio = pd.DataFrame(long_short_data)
+                return long_short_ratio
                 
         elif request.status == 429:
             print("API limit broken")
